3ec1b08655a71360b531b01ce7f512b66773bbff
kamioka24 <keee2404@gmail.com>
2021-04-19 17:07:34 +0900

    [Add]

--- app/src/main/java/com/example/basematch_k/TopActivity.kt
+++ app/src/main/java/com/example/basematch_k/TopActivity.kt
@@ -11,23 +11,27 @@ class TopActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top)
 
+        // プレイヤーログインボタン
         playerLoginButton.setOnClickListener {
             val intent = Intent(this, PlayerTeamIndexActivity::class.java)
             startActivity(intent)
         }
 
+        // プレイヤー新規登録ボタン
         playerRegistrationButton.setOnClickListener {
             val intent = Intent(this, PlayerTeamIndexActivity::class.java)
             startActivity(intent)
         }
 
+        // チームログインボタン
         teamLoginButton.setOnClickListener {
-            val intent = Intent(this, PlayerTeamIndexActivity::class.java)
+            val intent = Intent(this, TeamTeamIndexActivity::class.java)
             startActivity(intent)
         }
 
+        // チーム新規登録ボタン
         teamRegistrationButton.setOnClickListener {
-            val intent = Intent(this, PlayerTeamIndexActivity::class.java)
+            val intent = Intent(this, TeamTeamIndexActivity::class.java)
             startActivity(intent)
         }
     }
