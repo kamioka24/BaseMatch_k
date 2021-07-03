af69d1b3cf07048d1bb774cb185eb97aebc70cb6
kamioka24 <keee2404@gmail.com>
2021-07-03 11:28:58 +0900

    [Update] アクションバー排除 & [Add] 自動遷移

--- app/src/main/java/com/example/basematch_k/LaunchActivity.kt
+++ app/src/main/java/com/example/basematch_k/LaunchActivity.kt
@@ -3,9 +3,17 @@ package com.example.basematch_k
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_launch.*
 
 class LaunchActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+    private val runnable = Runnable {
+        val intent = Intent(this, TopActivity::class.java)
+        startActivity(intent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
@@ -14,5 +22,13 @@ class LaunchActivity : AppCompatActivity() {
             val intent = Intent(this, TopActivity::class.java)
             startActivity(intent)
         }
+
+        handler.postDelayed(runnable, 8000)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler.postDelayed(runnable, 8000)
     }
 }
